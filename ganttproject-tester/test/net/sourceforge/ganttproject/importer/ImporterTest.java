0ef9cc5009604b8b23722a28132493d3704b8c36
Joeri Exelmans <joeri.exelmans@gmail.com>
2020-05-29 16:07:09 +0200

    Extend ImporterTest with extra checks.

--- ganttproject-tester/test/net/sourceforge/ganttproject/importer/ImporterTest.java
+++ ganttproject-tester/test/net/sourceforge/ganttproject/importer/ImporterTest.java
@@ -1,11 +1,13 @@
 package net.sourceforge.ganttproject.importer;
 
 import biz.ganttproject.core.calendar.ImportCalendarOption;
+import net.sourceforge.ganttproject.CustomPropertyDefinition;
 import net.sourceforge.ganttproject.project.Project;
 import net.sourceforge.ganttproject.resource.HumanResourceMerger;
 import net.sourceforge.ganttproject.test.ProjectTestBase;
 
 import java.io.IOException;
+import java.util.List;
 
 import static biz.ganttproject.core.calendar.ImportCalendarOption.Values.*;
 import static net.sourceforge.ganttproject.resource.HumanResourceMerger.MergeResourcesOption.*;
@@ -14,6 +16,7 @@ public class ImporterTest extends ProjectTestBase {
     public void testImport() throws IOException {
         Project target = getTestProject("/testproject.xml");
 
+        // We start with 2 resources
         assertEquals(2, target.getHumanResourceManager().getResources().size());
 
         // Project with a single resource defined
@@ -26,6 +29,17 @@ public class ImporterTest extends ProjectTestBase {
 
         ImporterFromGanttFile.importProject(target, source, mergeOption, calendarOption);
 
+        // 1 resource was imported
         assertEquals(3, target.getHumanResourceManager().getResources().size());
+
+        // Imported resource had a custom property "age" defined,
+        // this should be the only custom property in the merged project
+        List<CustomPropertyDefinition> actualCustomProperties = target.getHumanResourceManager().getCustomPropertyManager().getDefinitions();
+        assertEquals(1, actualCustomProperties.size());
+        assertEquals("age", actualCustomProperties.get(0).getName());
+
+        // Now check if the property is correctly set on the imported resource
+        assertEquals("age", target.getHumanResourceManager().getResources().get(2).getCustomProperties().get(0).getDefinition().getName());
+        assertEquals("60", target.getHumanResourceManager().getResources().get(2).getCustomProperties().get(0).getValue());
     }
 }

4399fa26b8c192c986da4712c6066648668d0f3b
Joeri Exelmans <joeri.exelmans@gmail.com>
2020-06-03 20:28:48 +0200

    Fix NullPointerException in TestImportResources

--- ganttproject-tester/test/net/sourceforge/ganttproject/resource/TestImportResources.java
+++ ganttproject-tester/test/net/sourceforge/ganttproject/resource/TestImportResources.java
@@ -1,5 +1,6 @@
 package net.sourceforge.ganttproject.resource;
 
+import net.sourceforge.ganttproject.assignment.AssignmentManager;
 import net.sourceforge.ganttproject.resource.HumanResourceMerger.MergeResourcesOption;
 import net.sourceforge.ganttproject.task.CustomColumnsManager;
 import junit.framework.TestCase;
@@ -10,13 +11,14 @@ public class TestImportResources extends TestCase {
         MergeResourcesOption mergeOption = new MergeResourcesOption();
         mergeOption.setValue(MergeResourcesOption.BY_NAME);
 
-        HumanResourceManager mergeTo = new HumanResourceManager(null, null, new CustomColumnsManager());
-        mergeTo.add(new HumanResource("joe", 1, mergeTo, null));
-        mergeTo.add(new HumanResource("john", 2, mergeTo, null));
+        AssignmentManager am = new AssignmentManager();
+        HumanResourceManager mergeTo = new HumanResourceManager(am, null, new CustomColumnsManager());
+        mergeTo.add(new HumanResource("joe", 1, mergeTo, am));
+        mergeTo.add(new HumanResource("john", 2, mergeTo, am));
 
-        HumanResourceManager mergeFrom = new HumanResourceManager(null, null, new CustomColumnsManager());
-        mergeFrom.add(new HumanResource("jack", 1, mergeFrom, null));
-        mergeFrom.add(new HumanResource("joe", 2, mergeFrom, null));
+        HumanResourceManager mergeFrom = new HumanResourceManager(am, null, new CustomColumnsManager());
+        mergeFrom.add(new HumanResource("jack", 1, mergeFrom, am));
+        mergeFrom.add(new HumanResource("joe", 2, mergeFrom, am));
 
         mergeTo.importData(mergeFrom, new OverwritingMerger(mergeOption));
 
@@ -30,13 +32,14 @@ public class TestImportResources extends TestCase {
         MergeResourcesOption mergeOption = new MergeResourcesOption();
         mergeOption.setValue(MergeResourcesOption.BY_ID);
 
-        HumanResourceManager mergeTo = new HumanResourceManager(null, null, new CustomColumnsManager());
-        mergeTo.add(new HumanResource("joe", 1, mergeTo, null));
-        mergeTo.add(new HumanResource("john", 2, mergeTo, null));
+        AssignmentManager am = new AssignmentManager();
+        HumanResourceManager mergeTo = new HumanResourceManager(am, null, new CustomColumnsManager());
+        mergeTo.add(new HumanResource("joe", 1, mergeTo, am));
+        mergeTo.add(new HumanResource("john", 2, mergeTo, am));
 
-        HumanResourceManager mergeFrom = new HumanResourceManager(null, null, new CustomColumnsManager());
-        mergeFrom.add(new HumanResource("jack", 1, mergeFrom, null));
-        mergeFrom.add(new HumanResource("joe", 3, mergeFrom, null));
+        HumanResourceManager mergeFrom = new HumanResourceManager(am, null, new CustomColumnsManager());
+        mergeFrom.add(new HumanResource("jack", 1, mergeFrom, am));
+        mergeFrom.add(new HumanResource("joe", 3, mergeFrom, am));
 
         mergeTo.importData(mergeFrom, new OverwritingMerger(mergeOption));
 
